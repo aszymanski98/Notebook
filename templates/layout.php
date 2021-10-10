12a605d6d0a628f8f2c579a3ba9d56726f6a3e01
a.szymanski98 <a.szymanski@protonmail.com>
2021-10-10 14:18:02 +0200

    added styles

--- templates/layout.php
+++ templates/layout.php
@@ -6,30 +6,26 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Notebook</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="../public/style.css">
 </head>
 
 <body>
-  <h1 class="header">
-    My notes
-  </h1>
+  <div class="wrapper">
 
-  <ul>
-    <li>
-      <a href="/">Notes list</a>
-    </li>
+    <h1><i class="far fa-clipboard"></i>My notes</h1>
 
-    <li>
-      <a href="/?action=create">New note</a>
-    </li>
-  </ul>
+    <div class="container">
+      <ul>
+        <li><a href="/">Main page</a></li>
+        <li><a href="/?action=create">New note</a></li>
+      </ul>
 
-  <div class="container">
-    <?php include_once("pages/$page.php"); ?>
+      <div class="page">
+        <?php require_once("templates/pages/$page.php"); ?>
+      </div>
+    </div>
   </div>
-
-  <footer>
-
-  </footer>
 </body>
 
 </html>
