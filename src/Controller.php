d0d84cc398b8073ecc68e892e3dd0c85345e7f3d
a.szymanski98 <a.szymanski@protonmail.com>
2021-10-16 11:07:12 +0200

    adding notes to db

--- src/Controller.php
+++ src/Controller.php
@@ -17,6 +17,7 @@ class Controller
 
   private static array $configuration = [];
 
+  private Database $database;
   private array $request;
   private View $view;
 
@@ -31,7 +32,7 @@ class Controller
       throw new ConfigurationException("Configuration error");
     }
 
-    $db = new Database(self::$configuration['db']);
+    $this->database = new Database(self::$configuration['db']);
 
     $this->request = $request;
     $this->view = new View();
@@ -50,11 +51,8 @@ class Controller
 
         if (!empty($data)) {
           $created = true;
-
-          $viewParams = [
-            "title" => $data['title'],
-            "description" => $data['description'],
-          ];
+          $this->database->createNote($data);
+          header('Location: /');
         }
 
         $viewParams['created'] = $created;
--- src/Database.php
+++ src/Database.php
@@ -11,22 +11,47 @@ use App\Exception\ConfigurationException;
 use App\Exception\StorageException;
 use PDOException;
 use PDO;
+use Throwable;
 
 class Database
 {
+
+  private PDO $conn;
+
   public function __construct(array $config)
   {
     try {
       $this->validateConfig($config);
-
-      $connection = new PDO(
-        "mysql:dbname={$config['database']};host={$config['host']}",
-        $config['user'],
-        $config['password']
-      );
+      $this->createConnection($config);
     } catch (PDOException $e) {
       throw new StorageException('Connection error');
     }
+  }
+
+  public function createNote(array $data): void
+  {
+    try {
+      $title = $this->conn->quote($data["title"]);
+      $description = $this->conn->quote($data["description"]);
+
+      $query = "INSERT INTO notes(title, description) VALUES ($title, $description)";
+
+      $this->conn->exec($query);
+    } catch (Throwable $e) {
+      throw new StorageException('Failed to create new note', 400, $e);
+    }
+  }
+
+  private function createConnection(array $config): void
+  {
+    $this->conn = new PDO(
+      "mysql:dbname={$config['database']};host={$config['host']}",
+      $config['user'],
+      $config['password'],
+      [
+        PDO::ERRMODE_EXCEPTION,
+      ]
+    );
   }
 
   private function validateConfig(array $config): void
